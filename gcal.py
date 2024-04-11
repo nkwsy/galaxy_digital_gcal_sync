4cbe550cbb21e15987329482875aae59e5602412
nkwsy <nick@nickwesley.me>
2024-04-11 00:39:28 -0500

    upped the gcal api to return 2500 tokens, added graceful fail for dupe events

--- gcal.py
+++ gcal.py
@@ -7,6 +7,8 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
 
 # If modifying these SCOPES, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -47,46 +49,53 @@ def change_color(attendees):
 #TODO: Add location to calendar event, pull from galaxy digital API
 #TODO: Add description to calendar event, pull from galaxy digital API
 def update_calendar_events(shifts, service, calendar_id='primary', add_attendees=False):
-    events = service.events().list(calendarId=calendar_id).execute()
+    events = service.events().list(calendarId=calendar_id, maxResults=2500).execute()
     logger.info(f"Updating calendar {calendar_id}")
     logger.debug(f"Events: {events}")
     for shift in shifts:
-        # Check to see if duration of shift is greater than 24 hours
-        time_delta = shift['end_time'] - shift['start_time']
-        if time_delta.days > 1:
-            logger.error(f"Shift {shift['id']} is longer than 24 hours, skipping")
-            continue
-        event_id = shift['id']
-        event = {
-            'id': event_id,
-            'summary': f"{shift['title']} - ({shift['slots_filled']}/{shift['slots']}) ",
-            'start': {
-                'dateTime': shift['start_time'].isoformat(),
-                'timeZone': 'America/Chicago',
-            },
-            'end': {
-                'dateTime': shift['end_time'].isoformat(),
-                'timeZone': 'America/Chicago',
-            },
-            'colorId': change_color(shift['slots_filled']),
-        }
-        # Get the list of event attendees
-        if add_attendees:
-            # attendees = [{'email': user['user_email']} for user in shift['users']]
-            # event['attendees'] = attendees
-            event['attendees'] = {'email': 'test@urbanriv.org'}
-        else:
-            event['description'] = create_attendees_list(shift['users'])
-        # Check if event exists
-        existing_event = next((e for e in events['items'] if e['id'] == event_id), None)
-        if existing_event:
-            # Update the event
-            logger.info(f"Updating event {event_id}, {event}")
-            service.events().update(calendarId=calendar_id, eventId=event_id, body=event).execute()
-        else:
-            # Create new event
-            logger.info(f"Creating event {event_id}, {event}")
-            service.events().insert(calendarId=calendar_id, body=event).execute()
+            # Check to see if duration of shift is greater than 24 hours
+            time_delta = shift['end_time'] - shift['start_time']
+            if time_delta.days > 1:
+                logger.error(f"Shift {shift['id']} is longer than 24 hours, skipping")
+                continue
+            event_id = shift['id']
+            event = {
+                'id': event_id,
+                'summary': f"{shift['title']} - ({shift['slots_filled']}/{shift['slots']}) ",
+                'start': {
+                    'dateTime': shift['start_time'].isoformat(),
+                    'timeZone': 'America/Chicago',
+                },
+                'end': {
+                    'dateTime': shift['end_time'].isoformat(),
+                    'timeZone': 'America/Chicago',
+                },
+                'colorId': change_color(shift['slots_filled']),
+            }
+            # Get the list of event attendees
+            if add_attendees:
+                # attendees = [{'email': user['user_email']} for user in shift['users']]
+                # event['attendees'] = attendees
+                event['attendees'] = [{'email': 'test@urbanriv.org'}]
+            else:
+                event['description'] = create_attendees_list(shift['users'])
+            # Check if event exists
+            existing_event = next((e for e in events['items'] if e['id'] == event_id), None)
+            if existing_event:
+                # Update the event
+                logger.info(f"Updating event {event_id}, {event}")
+                service.events().update(calendarId=calendar_id, eventId=event_id, body=event).execute()
+            else:
+                try:
+                    # Create new event
+                    logger.info(f"Creating event {event_id}, {event}")
+                    service.events().insert(calendarId=calendar_id, body=event).execute()
+                except HttpError as error:
+                    if error.resp.status == 409:
+                        logger.error(f"Event with this ID already exists. Consider updating it instead.")
+                    else:
+                        logger.error(f"Failed to create event {event_id}: {error}")
+    
 
 #TODO: Get updates from calendar and reflect in galaxy digital i.e. if a user declines a shift, update galaxy digital
 def get_calendars(service):
